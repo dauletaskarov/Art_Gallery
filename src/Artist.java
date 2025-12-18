45236b06499441d063a89241bda1e579c40a0b37
dauletaskarov <dauletaskarov561@gmail.com>
2025-12-18 13:56:26 +0500

    Artist.java

--- src/Artist.java
+++ src/Artist.java
@@ -1,26 +1,25 @@
 public class Artist {
 
-    // Attributes (fields)
+    
     private String name;
     private String country;
 
-    // Constructor
     public Artist(String name, String country) {
         this.name = name;
         this.country = country;
     }
 
-    // Getter for name
+
     public String getName() {
         return name;
     }
 
-    // Getter for country
+
     public String getCountry() {
         return country;
     }
 
-    // Method to display artist info
+
     public void displayInfo() {
         System.out.println("Artist name: " + name + ", Country: " + country);
     }
