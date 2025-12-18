2f2fa0ccf057ffb6624667210da7cfe778e1f783
dauletaskarov <dauletaskarov561@gmail.com>
2025-12-18 13:57:00 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Create gallery
+
         ArtGallery gallery = new ArtGallery("Modern Art Gallery", "Paris");
 
-        // Create artist
+ 
         Artist artist1 = new Artist("Vincent van Gogh", "Netherlands");
         Artist artist2 = new Artist("Vincent van Gogh", "Netherlands");
 
-        // Create artworks
+
         Artwork artwork1 = new Artwork("Starry Night", 1889, 1000000);
         Artwork artwork2 = new Artwork("Starry Night", 1889, 1000000);
 
-        // Display information
+    
         gallery.displayInfo();
         artist1.displayInfo();
         artwork1.displayInfo();
 
-        // Compare artists
         if (artist1.getName().equals(artist2.getName()) &&
                 artist1.getCountry().equals(artist2.getCountry())) {
             System.out.println("Artists are the same.");
@@ -25,7 +24,6 @@ public class Main {
             System.out.println("Artists are different.");
         }
 
-        // Compare artworks
         if (artwork1.getTitle().equals(artwork2.getTitle()) &&
                 artwork1.getYear() == artwork2.getYear()) {
             System.out.println("Artworks are the same.");
